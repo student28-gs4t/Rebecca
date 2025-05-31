00befdb49fcf23891db1ee847abf5b4829785780
Rebecca Wong <student28.gs4t@gmail.com>
2025-05-31 14:50:12 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Rebecca";
+let age = 13;
+let likesSushi = true;
+
+function greeting(name) {
+    console.log('Hello', name);
+}
+
+greeting('Rebecca');
+
+let Newname = document.getElementById("greeter").value;
